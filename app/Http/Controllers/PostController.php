575eefcb22af64d6da69b677b802b224e025fa7e
Arvy Renardi <renardiarvy@gmail.com>
2020-07-06 15:55:36 +0700

    02 Read API Endpoint

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{   
+    public function index()
+    {
+        $data = Post::all();
+        return response()->json($data, 200);
+    }
+
+    public function show($id)
+    {
+        $data = Post::find($id);
+        return response()->json($data, 200);
+    }
+
+}
